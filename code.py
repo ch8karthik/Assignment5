f1d2b1a1acb8aeeaff95bf9b25c4aaa521e43406
karthikchapalamadugu <56600914+karthikchapalamadugu@users.noreply.github.com>
2021-11-22 16:29:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Source code for summation of two numbers
+# Source code for subtraction of two numbers
 
-def subtraction(data):
-    return sub(data)
+def subtraction(a,b):
+    return (a-b)
